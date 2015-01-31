c7b364839a23e44d67d67b493cea9018bc122053
n8chz <n8chz@yahoo.ca>
2015-01-31 16:45:27 -0500

    Corrected word wrap in textarea

--- data/selection.js
+++ data/selection.js
@@ -1,4 +1,7 @@
 self.on("click", function () {
   var selectionText = document.getSelection().toString();
-  self.postMessage(selectionText.replace(/[^ -~]/g, "").replace(/\s{3,}/g, "  ")).trim();
+  console.log("before: "+JSON.stringify(selectionText));
+  console.log("after replacing \\n: "+JSON.stringify(selectionText.replace(/\n/, " ")));
+  self.postMessage(selectionText.replace(/\n/g, " ").replace(/[^ -~]/g, "").replace(/\s{3,}/, "  ").trim());
+  // self.postMessage(selectionText.replace(/\n/, " ").replace(/[^ -~]/g, "").trim());
 });
